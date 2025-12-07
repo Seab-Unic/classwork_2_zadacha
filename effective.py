3108277cb6409419ef55fb5d6f227190fd4fe0f7
Seaborgium_element <evskv42@yandex.ru>
2025-12-07 20:56:35 +0700

    Update and rename Read_it_only.py to effective.py

---
+++ effective.py
@@ -0,0 +1,11 @@
+import random
+dbl1 = [random.randint(1, 2345), random.randint(1, 2345)]
+dbl2 = [random.randint(1, 2345), random.randint(1, 2345)]
+dbl3 = [random.randint(1, 2345), random.randint(1, 2345)]
+dbl4 = [random.randint(1, 2345), random.randint(1, 2345)]
+max1, max2, max3, max4 = max(dbl1), max(dbl2), max(dbl3), max(dbl4)
+lusm = max1 + max2 + max3 + max4
+if lusm % 5 == 0:
+    lusm -= 1
+print(lusm)
+
